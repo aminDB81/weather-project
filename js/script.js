036e3aa929af2330518689ba140f99a2a58acbf6
aminDB81 <aminbaluchi67@gmail.com>
2023-08-13 22:08:54 +0330

    Creating required variables and  items

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 const apiKey = "2d1685d839accc8cfbcdf0aa47378c6b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=2d1685d839accc8cfbcdf0aa47378c6b&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+checkWeather()
